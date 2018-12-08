5a5461b84cdebd7e53695eb51b0c2b15704dcb05
jaysattar85 <junaid.sattar85@gmail.com>
2018-12-08 12:10:24 -0500

    Test Commit

---
+++ src/main/java/PracticeSyntax/Practice/Test.java
@@ -0,0 +1,11 @@
+package PracticeSyntax.Practice;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello this is my Test");
+
+	}
+
+}
---
+++ src/main/java/PracticeSyntax/Practice/Test2.java
@@ -0,0 +1,11 @@
+package PracticeSyntax.Practice;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello this is my Test");
+
+	}
+
+}
